bfb395518aeb9b69882c1edd4d0098fe28116b5b
Nodirasdrv <nodirasdrv@gmail.com>
2022-02-08 15:04:43 +0600

    first commit

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+# Git
+# write in terminal
+# git init - connects the folder with git repository
+# git status - shows the status of the file
+# git add (name of the file)
+# git add . (adds every file)
+# git rm --cached (.filename) - will not upload on git
+# git reset --hard HEAD - откат
+# rm - rf .git - deletes git
+# git pull - pulls deleted branch
